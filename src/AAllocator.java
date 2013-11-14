3ee488413ea5cd396517727c0654ed2c13511463
OdetteDu <Odette.Du@rice.edu>
2013-11-14 17:46:09 -0600

    remove unnecessary

--- src/AAllocator.java
+++ src/AAllocator.java
@@ -24,10 +24,10 @@ public class AAllocator {
 
 	private void rename(Register r, HashMap<Integer, Integer> renameList)
 	{
-		if(renameList.get(r.getVr())!=null)
+		if(renameList.get(r.getNumber())!=null)
 		{
-			int newName=renameList.get(r.getVr());
-			r.setVr(newName);
+			int newName=renameList.get(r.getNumber());
+			r.setNumber(newName);
 			rename(r,renameList);
 		}
 	}
@@ -37,101 +37,63 @@ public class AAllocator {
 		HashMap<Integer, Integer> liveRegisters=new HashMap<Integer,Integer>();
 		ArrayList<Integer> definedVr=new ArrayList<Integer>();
 		HashMap<Integer, Integer> renameList=new HashMap<Integer,Integer>();
-		HashMap<Integer, Integer> registerUse=new HashMap<Integer, Integer>();
 
 		int i=instructions.size()-1;
 		while(i>=0)
 		{
 			Instruction instruction=instructions.get(i);
-
 			if(instruction.getTarget()!=null)
 			{
 				Register target=instruction.getTarget();
-
 				rename(target,renameList);
-
-				if(liveRegisters.get(target.getVr())!=null)
+				if(liveRegisters.get(target.getNumber())!=null)
 				{
-					int lastUse=liveRegisters.get(target.getVr());
-					target.setLastUse(lastUse);
-					liveRegisters.remove(target.getVr());
-					target.setNextUse(registerUse.get(target.getVr()));
-					registerUse.remove(target.getVr());
-
+					liveRegisters.remove(target.getNumber());
 				}
 				else
 				{
-					if(definedVr.contains(target.getVr()))
+					if(definedVr.contains(target.getNumber()))
 					{
 						int availableName=getAvailableName();
-						renameList.put(target.getVr(),availableName);
-						target.setVr(availableName);
+						renameList.put(target.getNumber(),availableName);
+						target.setNumber(availableName);
 					}
-					target.setLastUse(-1);
-					target.setNextUse(-1);
-				}
 
-				definedVr.add(target.getVr());
-				target.setDefine(i);
+				}
+				definedVr.add(target.getNumber());
 			}
 
 			if(instruction.getSource1()!=null)
 			{
 				Register source1=instruction.getSource1();
-
 				rename(source1,renameList);
-
-				if(liveRegisters.get(source1.getVr())!=null)
+				if(liveRegisters.get(source1.getNumber())==null)
 				{
-					int lastUse=liveRegisters.get(source1.getVr());
-					source1.setLastUse(lastUse);
-					source1.setNextUse(registerUse.get(source1.getVr()));
-					registerUse.put(source1.getVr(), i);
-				}
-				else
-				{
-					if(definedVr.contains(source1.getVr()))
+					if(definedVr.contains(source1.getNumber()))
 					{
 						int availableName=getAvailableName();
-						renameList.put(source1.getVr(),availableName);
-						source1.setVr(availableName);
+						renameList.put(source1.getNumber(),availableName);
+						source1.setNumber(availableName);
 					}
-					source1.setLastUse(i);
-					liveRegisters.put(source1.getVr(), i);
-					source1.setNextUse(i);
-					registerUse.put(source1.getVr(), i);
+					liveRegisters.put(source1.getNumber(), i);
 				}
 			}
 
 			if(instruction.getSource2()!=null)
 			{
 				Register source2=instruction.getSource2();
-
 				rename(source2,renameList);
-
-				if(liveRegisters.get(source2.getVr())!=null)
+				if(liveRegisters.get(source2.getNumber())==null)
 				{
-					int lastUse=liveRegisters.get(source2.getVr());
-					source2.setLastUse(lastUse);
-					source2.setNextUse(registerUse.get(source2.getVr()));
-					registerUse.put(source2.getVr(), i);
-				}
-				else
-				{
-					if(definedVr.contains(source2.getVr()))
+					if(definedVr.contains(source2.getNumber()))
 					{
 						int availableName=getAvailableName();
-						renameList.put(source2.getVr(),availableName);
-						source2.setVr(availableName);
+						renameList.put(source2.getNumber(),availableName);
+						source2.setNumber(availableName);
 					}
-					source2.setLastUse(i);
-					liveRegisters.put(source2.getVr(), i);
-					source2.setNextUse(i);
-					registerUse.put(source2.getVr(), i);
+					liveRegisters.put(source2.getNumber(), i);
 				}
-
 			}
-
 			i--;
 		}
 
--- src/Register.java
+++ src/Register.java
@@ -1,65 +1,24 @@
 
 public class Register {
 	
-	private int pr;
-	private int vr;
-	private int nextUse;
-	private int lastUse;
-	private int define;
+	private int number;
 
 	public Register(int vr)
 	{
-		this.vr=vr;
-		this.pr=-1;
-		this.nextUse=-1;
-		this.lastUse=-1;
-		this.define=-1;
+		this.number=vr;
 	}
 	
 	public String toString()
 	{
-		//return "(vr:"+getVr()+" pr:"+pr+" nextUse:"+getNextUse()+" lastUse:"+getLastUse()+" define:"+getDefine()+")";
-		//return "(vr:"+getVr()+" pr:"+pr+" nu:"+getNextUse()+" lu:"+getLastUse()+" d:"+getDefine()+")";
-		return "r"+vr;
+		return "r"+number;
 	}
 
-	public int getPr() {
-		return pr;
+	public int getNumber() {
+		return number;
 	}
 
-	public void setPr(int pr) {
-		this.pr = pr;
-	}
-
-	public int getVr() {
-		return vr;
-	}
-
-	public void setVr(int vr) {
-		this.vr = vr;
-	}
-
-	public int getNextUse() {
-		return nextUse;
-	}
-
-	public void setNextUse(int nextUse) {
-		this.nextUse = nextUse;
-	}
-
-	public int getLastUse() {
-		return lastUse;
-	}
-
-	public void setLastUse(int lastUse) {
-		this.lastUse = lastUse;
-	}
-
-	public int getDefine() {
-		return define;
-	}
-
-	public void setDefine(int define) {
-		this.define = define;
+	public void setNumber(int number) {
+		this.number = number;
 	}
 }
+
